8549d45e0a8b020a11ddcfd1fef717c3346fc82c
Elmura <erbakytelmura@gmail.com>
2024-11-11 10:49:32 +0500

    elmura exam four

---
+++ Ls/script.js
@@ -0,0 +1,66 @@
+//1 tapsrma
+
+// var ai = +prompt("1-12 deyin san engiziniz")
+
+// var mezgil
+
+// switch (ai)  {
+//   case 1:
+//   case 12:
+//   case 2:
+//        mezgil =",Winter"
+//        break;
+//   case 3:
+//   case 4:
+//   case 5:
+//         mezgil = ",Spring"
+//         break;
+//   case 6:
+//   case 7:
+//   case 8:
+//          mezgil = ",Summer"
+//        break;
+//   case 9:
+//   case 10:
+//   case 11:
+
+//            mezgil = ",Autumn"
+//           break;
+//   default:
+//    alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+//     break;
+// }
+
+//  alert("Ai:" +ai+ ","+"Маусым"+ mezgil)
+
+
+//2tapsrma
+// let a= prompt("A Санын енгізіңіз")
+// let b= prompt("B Санын енгізіңіз")
+
+// if (a>b) {
+//      alert("a sany b saninan ylkebn")
+// }
+// else if (b>a) {
+//      alert("b sany a sanynan ulken")
+// }
+// else if (a=b) {
+//      alert("a sany b sany ten")
+// }
+// alert("a men b sanynyn ortagy:" +(+a + +b)/2)
+
+
+//3tapsrma
+
+// var san1 = prompt("Бірінші санды енгізіңіз")
+// var san2 = prompt("Екінші санды енгізіңіз")
+// var san3 = prompt("Ұшыншы санды енгізіңіз")
+
+
+// if (san1 > san2 && san2 > san3) {
+//      alert("Ең үлке саннан ең кішкантай санның айырмасы:" + +(san1-san2))
+// }else if (san1 > san2&& san3 < san2){
+//      alert("Ең үлкен санмен ең кішісанның айырымы:" ++(san1-san2))
+// }else if ( san2 >san1 && san3 > san1) {
+//      alert("Ең үлкен санмен ең кіші санның айырмасы:" ++())
+// }
